a8ed0cef33b6f710ba151011ddd4922e69cdf4ff
isa <messaiytwitchr6@gmail.com>
2021-08-01 14:00:12 -0700

    moved a message inside of kills array

--- src/bot.js
+++ src/bot.js
@@ -7,11 +7,11 @@ require('dotenv').config()
 const client = new Client()
 //kill messages
 const kills = ["has drowned in money!", "got sniped!", "got poisoned!", "fell in the shower!", 
-    "got cancer!", "got ran over by a car!", "fell out of a plane!",
+    "got cancer!", "got ran over by a car!", "fell out of a plane!","lost ttheir phones!",
     "was eaten by a shark!", "remembered an embarrassing moment from 2 years ago!", "fell down the stairs!",
     "spontaneously died!", "didnt do their math homework!", "got electrocuted!", "got an F on their test!", 
     "got stabbed!", "stubbed their toe!", "power went out while updating BIOS!", "got hit by an avalanche!", 
-    "was caught lackin!", "got one tapped!", "got checkmated!", "got a virus!", "got forced to watch twilight!", "lost ttheir phones!",]
+    "was caught lackin!", "got one tapped!", "got checkmated!", "got a virus!", "got forced to watch twilight!",]
 //get random values
 random_item=(kill)=>kill[Math.floor(Math.random()*kill.length)]
 //bot ready message and status
