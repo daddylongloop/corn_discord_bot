c5358357c0a8cca87985888144c6d2826f7d1473
isa <messaiytwitchr6@gmail.com>
2021-10-02 17:37:21 -0700

    forgot to delete some comments

--- src/bot.js
+++ src/bot.js
@@ -11,15 +11,8 @@ const kills = require("./kills")
 //kill messages
 let helpMsg = `use "!pls kill" in chat. you can @ other members to kill them or you can use "!pls kill me" to kill yourself. \
  You can also use !pls join and!pls leave for voice chat commands(still need to implement)`
-
-// const kills = ["has drowned in money!", "got sniped!", "got poisoned!", "fell in the shower!", 
-//     "got cancer!", "got ran over by a car!", "fell out of a plane!","lost their phone!",
-//     "was eaten by a shark!", "remembered an embarrassing moment from 2 years ago!", "fell down the stairs!",
-//     "spontaneously died!", "didnt do their math homework!", "got electrocuted!", "got an F on their test!", 
-//     "got stabbed!", "stubbed their toe!", "power went out while updating BIOS!", "got hit by an avalanche!", 
-//     "was caught lackin!", "got one tapped!", "got checkmated!", "got a virus!", "got forced to watch twilight!",
-//     "got bit by a zombie!", "got bit by vampires!", "was the last one to finish a lap during pe!(fatty)"]
-//get rbandom values
+ 
+//get random values
 random_item=(kill)=>kill[Math.floor(Math.random()*kill.length)]
 //bot ready message and status
 client.on('ready',()=>{
