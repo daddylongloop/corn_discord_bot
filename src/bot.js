fa487277d574e31fc7135c6ddf01d68bc4ae4048
isa <messaiytwitchr6@gmail.com>
2021-10-02 17:36:49 -0700

    refactoring code and overall structure

--- src/bot.js
+++ src/bot.js
@@ -3,18 +3,23 @@ https://discord.com/api/oauth2/authorize?client_id=819374796261294110&permission
 */
 //prerequisites
 const { Client } = require('discord.js')
+const ytdl = require("ytdl-core");
 require('dotenv').config()
 const client = new Client()
+const { prefix } = require("../config.json");
+const kills = require("./kills")
 //kill messages
-const helpMsg = `use "!pls kill" in chat. you can @ other members to kill them or you can use "!pls kill me" to kill yourself. You can also use !pls join and!pls leave for voice chat commands(still need to implement)`
-const kills = ["has drowned in money!", "got sniped!", "got poisoned!", "fell in the shower!", 
-    "got cancer!", "got ran over by a car!", "fell out of a plane!","lost their phone!",
-    "was eaten by a shark!", "remembered an embarrassing moment from 2 years ago!", "fell down the stairs!",
-    "spontaneously died!", "didnt do their math homework!", "got electrocuted!", "got an F on their test!", 
-    "got stabbed!", "stubbed their toe!", "power went out while updating BIOS!", "got hit by an avalanche!", 
-    "was caught lackin!", "got one tapped!", "got checkmated!", "got a virus!", "got forced to watch twilight!",
-    "got bit by a zombie!", "got bit by vampires!", "was the last one to finish a lap during pe!(fatty)"]
-//get random values
+let helpMsg = `use "!pls kill" in chat. you can @ other members to kill them or you can use "!pls kill me" to kill yourself. \
+ You can also use !pls join and!pls leave for voice chat commands(still need to implement)`
+
+// const kills = ["has drowned in money!", "got sniped!", "got poisoned!", "fell in the shower!", 
+//     "got cancer!", "got ran over by a car!", "fell out of a plane!","lost their phone!",
+//     "was eaten by a shark!", "remembered an embarrassing moment from 2 years ago!", "fell down the stairs!",
+//     "spontaneously died!", "didnt do their math homework!", "got electrocuted!", "got an F on their test!", 
+//     "got stabbed!", "stubbed their toe!", "power went out while updating BIOS!", "got hit by an avalanche!", 
+//     "was caught lackin!", "got one tapped!", "got checkmated!", "got a virus!", "got forced to watch twilight!",
+//     "got bit by a zombie!", "got bit by vampires!", "was the last one to finish a lap during pe!(fatty)"]
+//get rbandom values
 random_item=(kill)=>kill[Math.floor(Math.random()*kill.length)]
 //bot ready message and status
 client.on('ready',()=>{
@@ -25,36 +30,21 @@ client.on('ready',()=>{
 client.on('message', (message) => { 
     if (message.channel.type === 'dm') return
     if (message.author.bot) return
+    if (!message.content.startsWith(prefix)) return;
 })
 //bot message reactions
-client.on('message', (message) =>{
-    let mention = message.mentions.users.first()
-    console.log(`${message.author.tag} has typed "${message.content}" in the "${message.channel.name}" channel inside of "${message.guild.name}" server`)
-    if (message.content === '!pls help') {
-    message.reply(helpMsg)
-    } 
-    if(message.content.startsWith("!pls kill")&&mention){message.channel.send(`${mention} ${random_item(kills)}`)}
-    if (message.content === '!pls kill me') { message.reply(random_item(kills)) }
-})
 client.on('message', (message) => {
-    if (message.content === '!pls join') {
-        if (!message.member.voice.channel) {
-            message.reply("you are not in a voice channel")
-            return
-        }
-        if (message.member.voice.channel) {
-            message.member.voice.channel.join()
-        }
+    if (message.content === '!pls kill me') { message.reply(random_item(kills)) }
+    if (message.content === `${prefix} help`) {message.reply(helpMsg)}
+    if (message.content === `${prefix} kill @everyone`) {
+        message.channel.send(`@everyone ${random_item(kills)}`)
     }
-    if (message.content === '!pls leave') {
-        if (!message.member.voice.channel) {
-            message.reply("you are not in a voice channel")
-            return
-        }
-        if (message.member.voice.channel) {
-            message.member.voice.channel.leave()
-        }
-    }
+    let mention = message.mentions.users.array()
+    if (message.content.startsWith("!pls kill") && mention.length == 1) { message.channel.send(`${mention} ${random_item(kills)}`) }
+    else if (message.content.startsWith(`${prefix}`) && mention.length != 1 || 0) {
+        mention.forEach(ment => {
+        message.channel.send(`${ment} ${random_item(kills)}`)
+    })}
 })
 //bot login
 client.login(process.env.token)
---
+++ src/kills.js
@@ -0,0 +1,9 @@
+//holds the array of kill messages
+const kills = ["has drowned in money!", "got sniped!", "got poisoned!", "fell in the shower!",
+    "got cancer!", "got ran over by a car!", "fell out of a plane!","lost their phone!",
+    "was eaten by a shark!", "remembered an embarrassing moment from 2 years ago!", "fell down the stairs!",
+    "spontaneously died!", "didnt do their math homework!", "got electrocuted!", "got an F on their test!", 
+    "got stabbed!", "stubbed their toe!", "power went out while updating BIOS!", "got hit by an avalanche!", 
+    "was caught lackin!", "got one tapped!", "got checkmated!", "got a virus!", "got forced to watch twilight!",
+    "got bit by a zombie!", "got bit by vampires!", "was the last one to finish a lap during pe!(fatty)"]
+module.exports = kills
